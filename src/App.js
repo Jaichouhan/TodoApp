63ecc5068da0930ab9b48043a5ef48d5a994a2ea
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-07-18 17:02:00 +0530

    create app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./Components/Todo";
+
+const App = () => {
+  return (
+    <div className="col-md-6">
+      <div className="set_todo">
+        <Todo />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Todo.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./Todo.css";
+
+const Todo = () => {
+  const [input, setInput] = useState("");
+  const [msg, setMsg] = useState([]);
+
+  const addTodo = (input) => {
+    const newTodo = [...msg, { input }];
+    setMsg(newTodo);
+  };
+
+  const submitTodo = (e) => {
+    e.preventDefault();
+    if (!input) return;
+    addTodo(input);
+    setInput("");
+  };
+
+  const removeTodo = (index) => {
+    const removeItem = [...msg];
+    removeItem.splice(index, 1);
+    setMsg(removeItem);
+  };
+
+  return (
+    <>
+      <h1 className="todo_h1">Todo App</h1>
+      <div className="todo_bg">
+        <form onSubmit={submitTodo}>
+          <input
+            type="text"
+            placeholder="type anything ...."
+            className="todo_input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button type="submit" className="todo_i">
+            {" "}
+            <i className="fa fa-plus "></i>
+          </button>
+        </form>
+      </div>
+      <div className="todo_div">
+        {msg &&
+          msg.map((data, index) => (
+            <div className="todo_name" key={index}>
+              <p>{data.input}</p>
+              <i className="fa fa-remove" onClick={() => removeTodo(index)}></i>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default Todo;
